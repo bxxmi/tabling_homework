b1b2eb52ca97a2d0651112e2e4b85a4601337085
bxxmi <bmi6638@gmail.com>
2022-03-13 14:44:14 +0900

    refactor: separate status logic

--- src/components/ReserveView/ReserveDetail/ReserveDetail.js
+++ src/components/ReserveView/ReserveDetail/ReserveDetail.js
@@ -1,21 +1,23 @@
-import styles from "./ReserveDetail.module.scss";
+import "./ReserveDetail.scss";
+import { reserveStatusRegex } from "../../../utils/statusRegex";
 
 const ReserveDetail = ({ target, selectedList }) => {
-  const { id, customer, menus, status, tables, timeRegistered, timeReserved } =
-    selectedList[0];
+  const { customer, status, timeRegistered, timeReserved } = selectedList[0];
 
   const detailContainer = document.createElement("div");
   const reserveInfoContainer = document.createElement("div");
   const bookerInfoContainer = document.createElement("div");
+  const button = document.createElement("button");
 
   detailContainer.className = "reserve_detail";
-  reserveInfoContainer.className = styles.reserve_detail_info;
-  bookerInfoContainer.className = styles.reserve_detail_booker;
+  reserveInfoContainer.className = "reserve_detail_info";
+  bookerInfoContainer.className = "reserve_detail_booker";
+  button.innerText = "닫기";
 
   reserveInfoContainer.innerHTML = `
     <h2>예약 정보</h2>
-    <div class=${styles.reserve_info}>
-      <p><span>예약 상태</span>${status}</p>
+    <div class="reserve_info">
+      <p><span>예약 상태</span>${reserveStatusRegex(status)}</p>
       <p><span>예약 시간</span>${timeReserved}</p>
       <p><span>예약 시간</span>${timeRegistered}</p>
     </div>
@@ -23,14 +25,15 @@ const ReserveDetail = ({ target, selectedList }) => {
 
   bookerInfoContainer.innerHTML = `
     <h2>고객 정보</h2>
-    <div class=${styles.booker_info}>
-    <p><span>고객 성명</span>${customer.name}</p>
-    <p><span>고객 등급</span>${customer.level}</p>
-    <p><span>고객 메모</span>${customer.memo}</p>
-    <p><span>요청사항</span>${customer.request}</p>
+    <div class="booker_info">
+    <p><span>고객 성명</span><span>${customer.name}</span></p>
+    <p><span>고객 등급</span><span>${customer.level}</span></p>
+    <p><span>고객 메모</span><span>${customer.memo}</span></p>
+    <p><span>요청사항</span><span>${customer.request}</span></p>
   </div>
   `;
 
+  detailContainer.appendChild(button);
   detailContainer.appendChild(reserveInfoContainer);
   detailContainer.appendChild(bookerInfoContainer);
   target.appendChild(detailContainer);
